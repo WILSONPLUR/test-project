70c25117b523fa1faee7af9a0054f22d1beb24b4
WILSONPLUR <yourshows12@gmail.com>
2023-03-03 23:00:56 +0300

    test task:added remove geometry function

--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
-// import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-// import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-
 const scene = new THREE.Scene();
-const light = new THREE.DirectionalLight(0x404040, 0.5);
+const directionalLight = new THREE.DirectionalLight("#000", 0.5);
+scene.add(directionalLight);
 const material = new THREE.MeshBasicMaterial({ color: "#BBBBBB" });
 scene.background = new THREE.Color("#ccc");
 
-scene.add(light);
 const camera = new THREE.PerspectiveCamera(
   45,
   window.innerWidth / window.innerHeight,
@@ -25,13 +22,7 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// camera.position.z = 10;
-// camera.position.x = 2;
-
 const controls = new OrbitControls(camera, renderer.domElement);
-// controls.enableDamping = true;
-// controls.dampingFactor = 0.05;
-// controls.maxPolarAngle = Math.PI / 2;
 
 camera.position.set(2, 2, 2);
 controls.target.set(0, 0, 0);
@@ -47,118 +38,75 @@ document.getElementById("scale").addEventListener("input", (e) => {
   inputVal = e.target.value;
 });
 
-function createObject() {
-  if (document.getElementById("select").value === "cube") {
-    const geometry = new THREE.BoxGeometry(inputVal, inputVal, inputVal);
-    const cube = new THREE.Mesh(geometry, material);
-    const randomXPos = Math.floor(Math.random() * 6);
-    const randomYPos = Math.floor(Math.random() * 4);
-    scene.add(cube);
-    cube.position.setX(randomXPos);
-    cube.position.setY(randomYPos);
-    //     document.querySelector(".geometries-list").innerHTML =
-    //       scene.children.map((obj) => {
-    //         return `
-    // <div class='list-item'>
-    // <span>${obj.uuid}</span>
-    // <button onclick=${scene.remove(obj)}>X</button>
-    // </div>
-    // `;
-    //       });
-  } else if (document.getElementById("select").value === "sphere") {
-    var mesh = new THREE.Mesh(new THREE.SphereGeometry(inputVal), material);
-    //const sphere = new THREE.Mesh(geometry, material)
-    const randomXPos = Math.floor(Math.random() * 6);
-    const randomYPos = Math.floor(Math.random() * 2);
-    const randomZPos = Math.floor(Math.random() * 2);
-    scene.add(mesh);
-    mesh.position.set(randomXPos + 1, randomYPos, randomZPos);
-    //     document.querySelector(".geometries-list").innerHTML =
-    //       scene.children.map((obj) => {
-    //         return `
-    // <div class='list-item'>
-    // <span>${obj.uuid}</span>
-    // <button onclick=${scene.remove(obj)}>X</button>
-    // </div>
-    // `;
-    //       });
-  } else if (document.getElementById("select").value === "circle") {
-    const object = new THREE.Mesh(
-      new THREE.RingGeometry(10, 50, 20, 5, 0, Math.PI * 2),
-      material
-    );
-    scene.add(object);
+function createGeometry() {
+  let geometry;
+  let mesh;
+  let randomXPos;
+  let randomYPos;
+  switch (document.getElementById("select").value) {
+    case "cube":
+      geometry = new THREE.BoxGeometry(inputVal, inputVal, inputVal);
+      mesh = new THREE.Mesh(geometry, material);
+      randomXPos = Math.floor(Math.random() * 6);
+      randomYPos = Math.floor(Math.random() * 4);
+      scene.add(mesh);
+      mesh.position.setX(randomXPos);
+      mesh.position.setY(randomYPos);
+      break;
+
+    case "sphere":
+      geometry = new THREE.SphereGeometry(inputVal);
+      mesh = new THREE.Mesh(geometry, material);
+      randomXPos = Math.floor(Math.random() * 6);
+      randomYPos = Math.floor(Math.random() * 4);
+      scene.add(mesh);
+      mesh.position.setX(randomXPos);
+      mesh.position.setY(randomYPos);
+      break;
+
+    case "cone":
+      geometry = new THREE.ConeGeometry(5, 20, 32);
+      mesh = new THREE.Mesh(geometry, material);
+      randomXPos = Math.floor(Math.random() * 6);
+      randomYPos = Math.floor(Math.random() * 4);
+      scene.add(mesh);
+      break;
+    default:
+      alert("Choose geometry from select!");
   }
+
+  document.querySelector(".geometries-list").textContent = "";
+  scene.children.map((geometry) => {
+    if (geometry.type === "Mesh") {
+      const div = document.createElement("div");
+      div.classList.add("list-item");
+      const span = document.createElement("span");
+      const spanText = document.createTextNode(geometry.uuid);
+      span.appendChild(spanText);
+      const btn = document.createElement("button");
+      const btnText = document.createTextNode("X");
+      btn.appendChild(btnText);
+      btn.addEventListener("click", () => {
+        if (scene.remove) {
+          scene.remove(geometry);
+          document.querySelector(".geometries-list").removeChild(div);
+        }
+        renderer.render(scene, camera);
+      });
+      div.append(span);
+      div.append(btn);
+      document.querySelector(".geometries-list").appendChild(div);
+    }
+  });
 
   controls.addEventListener("change", () => {
     renderer.render(scene, camera);
   });
+  renderer.render(scene, camera);
   controls.update();
+  console.log(scene.children);
 }
 
 document.getElementById("btn").addEventListener("click", () => {
-  createObject();
+  createGeometry();
 });
-
-// const scene = new THREE.Scene();
-
-// const camera = new THREE.PerspectiveCamera(
-//   75,
-//   window.innerWidth / window.innerHeight,
-//   0.1,
-//   1000
-// );
-
-// camera.position.z = 5;
-
-// const scale = document.getElementById("scale");
-// const input = document.getElementById("scale");
-// const list = document.querySelector(".geometries-list");
-// const btn = document.getElementById("btn");
-// const select = document.getElementById("select");
-// let inputVal = null;
-
-// const createObjectByParams = (obj) => {
-//   const renderer = new THREE.WebGLRenderer();
-//   renderer.setSize(window.innerWidth, window.innerHeight);
-//   document.body.appendChild(renderer.domElement);
-//   let geometryObj = null;
-//   if (obj === "sphere") {
-//     const geometry = new THREE.SphereGeometry(
-//       inputVal,
-//       inputVal,
-//       inputVal
-//     );
-//     const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-//     const sphere = new THREE.Mesh(geometry, material);
-//     geometryObj = sphere;
-//   } else if (obj === "cube") {
-//     const geometry = new THREE.BoxGeometry(inputVal, inputVal, inputVal);
-//     const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-//     const cube = new THREE.Mesh(geometry, material);
-//     geometryObj = cube;
-//   }
-//   scene.add(geometryObj);
-//   list.innerHTML = scene.children.map((obj) => {
-//     return `
-//           <div class='list-item'>
-//               <span>${obj.uuid}</span>
-//               <button onclick=${scene.remove(obj)}>X</button>
-//           </div>
-//       `;
-//   });
-//   renderer.render(scene, camera);
-//   console.log(geometryObj);
-// };
-
-// btn.addEventListener("click", () => {
-//   createObjectByParams("cube");
-// });
-
-// input.addEventListener("input", (e) => {
-//   inputVal = e.target.value;
-// });
-
-// //   select.addEventListener("change", (e) => {
-// //     handleSelect(e.target.value);
-// //   });
